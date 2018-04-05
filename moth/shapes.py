64fcbc4cf29600bb5739500c58192a349beaa815
NoamBenelli <33618293+NoamBenelli@users.noreply.github.com>
2018-04-05 12:43:32 +0300

    Add files via upload

---
+++ moth/shapes.py
@@ -0,0 +1,11 @@
+import numpy as np
+def circle(array, x, y, r=5, color = (1,0,0)):
+    for i in range(int(y-r),int(y+r)):
+        for j in range(int(x-r),int(x+r)):
+            if np.sqrt((i-y)**2+((j-x)**2))<r :
+                array[i][j] = color
+
+
+
+def square(array,x, y, a=3, color = (0,1,0)):
+           array[y-a:y+a,x-a:x+a] = color
