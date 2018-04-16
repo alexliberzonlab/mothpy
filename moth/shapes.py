4b421402dee76e2d4d38d086b146ebc7f61a7c70
NoamBenelli <Benellinoam@gmail.com>
2018-04-16 15:29:28 +0300

    four trajectories is a one time test for displaying four competing moths.

--- moth/shapes.py
+++ moth/shapes.py
@@ -1,5 +1,5 @@
 import numpy as np
-def circle(array, x, y, r=5, color = (1,0,0)):
+def circle(array, x, y, r=3, color = (1,0,0)):
     for i in range(int(y-r),int(y+r)):
         for j in range(int(x-r),int(x+r)):
             if np.sqrt((i-y)**2+((j-x)**2))<r :
@@ -7,10 +7,8 @@ def circle(array, x, y, r=5, color = (1,0,0)):
 
 
 
-def square(array,x, y, a=3, color = (0,1,0)):
+def square(array,x, y, a=2, color = (0,1,0)):
            array[y-a:y+a,x-a:x+a] = color
 
 
-"""
-this message is only here to test that git is working plz ignore or delete
-"""
+
