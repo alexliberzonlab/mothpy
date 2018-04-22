3dd2061b3c87ed307527ce2f297ba259022740aa
NoamBenelli <Benellinoam@gmail.com>
2018-04-22 12:12:16 +0300

    a snapshot in which the simulation, documentation and kalman filtering are located on the same file, before starting to fracture those into different, shorter files

--- moth/Trajectories dispaly - Kalman.py
+++ moth/Trajectories dispaly - Kalman.py
@@ -10,6 +10,7 @@ __authors__ = 'Noam Benelli'
 
 
 import sys
+import pylab
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.colors
@@ -18,7 +19,7 @@ import models
 import processors
 import scipy.misc
 from demos import _close_handle,_set_up_figure,_simulation_loop
-from shapes import circle,square
+from shapes import circle,square , cx ,cy
 
 
 
@@ -50,9 +51,22 @@ class KalmanFilterLinear:
     self.current_prob_estimate = (np.eye(size)-kalman_gain*self.H)*predicted_prob_estimate
 
 
+def accelration_constraint(ax,ay,a_max):
+  if ax > a_max:
+    ax = a_max
+  elif ax < -a_max:
+    ax = -a_max
+    
+  if ay > a_max:
+    ay = a_max
+  elif ay < -a_max:
+    ay = -a_max
+  return np.matrix([[ax],[ay]])
+  
+
 
     
-def moth_demo(x_start = 450, y_start = 345, dt=0.01, t_max = 10, draw_iter_interval=1):
+def moth_demo(x_start = 450, y_start = 320, dt=0.01, t_max = 5, draw_iter_interval=1):
     """
     a copy of the concetration_array_demo with the moth actions integrated
     """
@@ -66,8 +80,8 @@ def moth_demo(x_start = 450, y_start = 345, dt=0.01, t_max = 10, draw_iter_inter
     times_list = [] # a list of the time it took different moths to reach the goal, for hist purposes
     del_list = [] # a list of the moth indices that finished the track and were deleted 
 
-    num_it=1 #number of iterations
-    dist_it=100 #distance on y axis between starting points on different iterations
+    num_it=10 #number of iterations
+    dist_it=10 #distance on y axis between starting points on different iterations
     for i in range(num_it):
         moth_dict["moth{0}".format(i)] = models.moth_modular(sim_region, x_start, y_start-i*dist_it)
         list_dict["moth_trajectory_list{0}".format(i)] = []
@@ -86,8 +100,6 @@ def moth_demo(x_start = 450, y_start = 345, dt=0.01, t_max = 10, draw_iter_inter
                                                        500, 1.)
     # display initial concentration field as image
     conc_array = array_gen.generate_single_array(plume_model.puff_array)
-    #set up text file to recored the trajectory as a string of tuples
-    file_name = "moth_trajectory" + "(" + str(x_start) + "," + str(y_start) + ")"
     
     # define update and draw functions
     def update_func(dt, t):
@@ -149,13 +161,15 @@ def moth_demo(x_start = 450, y_start = 345, dt=0.01, t_max = 10, draw_iter_inter
 
 
     #implement the Kalman filter:
+    #
     state_transition = np.matrix([[1,dt,0,0],[0,1,0,0],[0,0,1,dt],[0,0,0,1]])
-    control_matrix = np.matrix([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
+    control_matrix = np.matrix([[(dt**2)/2,0],[0,(dt**2)/2],[dt,0],[0,dt]]) # acceleration constraint
+    a_max = 0.01 
     control_vector = np.matrix([[0],[0],[0],[0]])
     observation_matrix = np.eye(4)
     initial_probability = np.eye(4)
     process_covariance =0.0001*np.eye(4) #A crucial factor in this process
-    measurement_covariance = np.eye(4)*0.1
+    measurement_covariance = np.eye(4)*0.1 #also crucial
     
     #a dictionary of lists of the kalman corrections - (x,y) only.
     kalman_dict = {}
@@ -163,21 +177,40 @@ def moth_demo(x_start = 450, y_start = 345, dt=0.01, t_max = 10, draw_iter_inter
     for i in range(num_it):
         kalman_dict["Kalman_list{0}".format(i)] = []   
         #initiate Kalman filter with our specific parameters
-        initial_state = np.matrix([[x_start],[0],[y_start],[0]])  #this should be redone to indclude different initial states for different moths     
+        initial_state = np.matrix([[x_start],[0],[y_start],[0]])       
         kf = KalmanFilterLinear(state_transition, control_matrix, observation_matrix, initial_state, initial_probability, process_covariance, measurement_covariance)
+        vx,vy =0,0 #initial velocities are 
         for j in range(1,len(list_dict["moth_trajectory_list{0}".format(i)])):
-            #This is where I should implement a condition for j==0
+            #There is no iteration for when j==0
             x = list_dict["moth_trajectory_list{0}".format(i)][j][0]
             y = list_dict["moth_trajectory_list{0}".format(i)][j][1]
             x_minus_1 =list_dict["moth_trajectory_list{0}".format(i)][j-1][0]
             y_minus_1 =list_dict["moth_trajectory_list{0}".format(i)][j-1][1]
+            vx_minus_1 = vx #this iterations vx_minus1 is the last iterations vx
+            vy_minus_1 = vy
             vx = (x - x_minus_1)/dt
             vy = (y - y_minus_1)/dt
             kx = (kf.GetCurrentState()[0,0])
             ky = (kf.GetCurrentState()[2,0])
+            #apply the accelaration constraint
+            ax=(vx-vx_minus_1)/dt
+            ay=(vy-vy_minus_1)/dt
+            control_vector = accelration_constraint(ax,ay,a_max)
             kf.Step(control_vector,np.matrix([[x],[vx],[y],[vy]]))
             kalman_dict["Kalman_list{0}".format(i)].append((kx,ky))
-            
+
+
+    #documantation:
+    #set up text file to recored different lists
+    file_name = "moth_trajectory" + "(" + str(x_start) + "," + str(y_start) + ")"
+    file = open(file_name + ".txt", 'w')
+    for i in range(num_it):
+      file.write("moth trajectory " + str(i))
+      file.write(str(diff_dict["diff_list{0}".format(i)])) 
+    file.close()
+
+
+    """        
     #graphics:       
     #create a matrix, insert all colored trajectories
     im = 1*np.ones((500,500,3))
@@ -213,7 +246,23 @@ def moth_demo(x_start = 450, y_start = 345, dt=0.01, t_max = 10, draw_iter_inter
 
     #save plot as image
     scipy.misc.imsave('simulation of ' + str(num_it)+' moths' +'.jpg', im)
+    """
     
+    #alternative graphic function - shows only kalman trajectories
+    color_wheel =['-b','-g','-c','-m','-y','-k']
+    for i in range(num_it): #present the different kalman trajectories
+      kzip = zip(*kalman_dict["Kalman_list{0}".format(i)])
+      kx,ky=kzip[0],kzip[1]
+      pylab.plot(kx,ky,color_wheel[i%6]) #choose a color from the color wheel for each trajectory
+      
+    pylab.plot(cx,cy,'-r')#add a circle for the odor source
+    pylab.ylim(0,500)
+    pylab.xlabel('X position')
+    pylab.ylabel('Y position')
+    pylab.title('competition between moths')
+    #pylab.legend(('calculated','kalman'))
+    pylab.legend(loc='upper left')  
+    pylab.show()
     print "done"
 
 moth_demo()
