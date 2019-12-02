6e0924b758201da988a3f4dac9559113fe695d20
NoamBenelli <33618293+NoamBenelli@users.noreply.github.com>
2019-12-02 11:33:47 +0200

    Update casting_competition.py
    Number of navigators per type per iteration changed from 60 to 1000

--- mothpy/casting_competition.py
+++ mothpy/casting_competition.py
@@ -26,35 +26,24 @@ def create_trajectory_data(job_file_name = 'job.json',data_file_name ='data.json
     navigators = []
     navigator_titles = []
 
-    """
-    def call_navigators(wait,cast,nav):
-        for j in range(10):
-            for i in range(20):
-                #print "navigator number " + str(j)
-                new_navigator = copy.copy(navigator1)
-                new_navigator.wait_type = wait
-                new_navigator.cast_type = cast
-                new_navigator.nav_type = nav
-                new_navigator.y = 450 - i*3
-                new_navigator.x = 499 - j
 
-    """
     def call_navigators(wait,cast,nav):
-        for j in range(60):
-            for i in range(200):
+        for j in range(20): #remember to turn this back to 20!
+            for i in range(50):
                 new_navigator = copy.copy(navigator1)
                 new_navigator.wait_type = wait
                 new_navigator.cast_type = cast
                 new_navigator.nav_type = nav
-                new_navigator.y = 600 - i
-                new_navigator.x = 499 - 2*j
-            title =  \
-                ' cast - ' + str(new_navigator.cast_type) \
-                + '; nav - ' + str(new_navigator.nav_type) \
-                + str(len(navigators))
+                new_navigator.y = 650 - i*4
+                new_navigator.x = 499 - j*2
+
+                title =  \
+                    ' cast - ' + str(new_navigator.cast_type) \
+                    + '; nav - ' + str(new_navigator.nav_type) \
+                    + str(len(navigators))
                 
-            navigators.append(new_navigator)
-            navigator_titles.append(title)
+                navigators.append(new_navigator)
+                navigator_titles.append(title)
     
     call_navigators(1,2,'alex')
     call_navigators(1,3,'alex')
